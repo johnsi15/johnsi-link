f2060af738eaa174a784da32d536e4d53bfd0535
John Serrano <andrey_201015@hotmail.com>
2024-08-15 21:17:17 -0500

    Changes config prettier format code

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,7 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
+  semi: false,
+  singleQuote: true,
   plugins: ["prettier-plugin-tailwindcss"],
 };
 
